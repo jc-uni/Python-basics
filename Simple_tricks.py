eb29934a68bd91c632d2fa2b21570c292f2865fc
jc-uni <jonas.claesson@unicus.no>
2021-12-03 13:37:14 +0100

    added the 4 datatypes

--- Simple_tricks.py
+++ Simple_tricks.py
@@ -3,16 +3,23 @@ from time import sleep
 #intro txt
 def print_some_stuff():
     print("Hello world" +"\r\n" +"wait...")
-    sleep(3)
+    sleep(1)
     print("This is python 3.9")
     various_data_types()
 
 def various_data_types():
     some_text = "a string, this is -Yoda"
     a_number = 1
-    a_list = [10, 12, 32, 51, a_number]
-    print(some_text +", a number: " +str(a_number))
+
+    a_list = [some_text, 10, 28, 10, 51, a_number, True, False] #ordered, mutable, allows duplicates
+    a_set = {some_text, 10, 28, 10, 51, a_number, True, False} #unordered, immutable (add/remove ok), disallows duplicates
+    a_tuple = (some_text, 10, 28, 10, 51, a_number, True, False) #ordered, immutable, allows duplicates
+    a_dictionary = {"brand": "Volvo", "model": "XC60", "year": 2019, "operable": True} #ordered(pr. python 3.7), mutable, disallows duplicates
+   
     print(a_list[:])
+    print(a_set)
+    print(a_tuple)
+    print(a_dictionary)
 
 
 
